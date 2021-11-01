e973357f8af0d366d42dbd1116b4708abe9dcf52
kaushalmeena <kaushal.meena@aasaanjobs.com>
2021-11-01 15:57:01 +0530

    feat(renderer): add favorite toggle

--- src/renderer/screens/Execute/index.tsx
+++ src/renderer/screens/Execute/index.tsx
@@ -1,11 +1,73 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Icon,
+  IconButton,
+  Stack,
+  Typography,
+  Box
+} from "@mui/material";
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router";
+import { updateFavoriteScriptField } from "../../database/main";
+import { useSnackbar } from "../../hooks/useSnackbar";
+import PageName from "../../shared/PageName";
+import { Params } from "../../types/router";
 
 const Execute = (): JSX.Element => {
+  const { showSnackbar } = useSnackbar();
+
+  const history = useHistory();
+  const params = useParams<Params>();
+
+  const [favorite, setFavorite] = useState(0);
+
+  const scriptId = Number(params.scriptId);
+
+  const handleDeleteClick = () => {
+    history.push(`/delete/${scriptId}`);
+  };
+
+  const handleUpdateClick = () => {
+    history.push(`/update/${scriptId}`);
+  };
+
+  const handleFavouriteToogle = () => {
+    const newFavoriteValue = 1 - favorite;
+    updateFavoriteScriptField(scriptId, newFavoriteValue)
+      .then(() => {
+        setFavorite(newFavoriteValue);
+      })
+      .catch((err) => {
+        console.error(err);
+        showSnackbar("Error occured while updating.", "error");
+      });
+  };
+
   return (
     <>
-      <Typography fontSize={28} fontWeight="400">
-        Execute
+      <PageName name="Execute" />
+      <Box marginBottom={2} display="flex" justifyContent="space-between">
+        <Stack direction="row" gap={1}>
+          <Button variant="contained" onClick={handleUpdateClick}>
+            Update
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleDeleteClick}
+          >
+            Delete
+          </Button>
+        </Stack>
+        <IconButton
+          color={favorite === 1 ? "secondary" : "primary"}
+          onClick={handleFavouriteToogle}
+        >
+          <Icon>{favorite === 1 ? "favorite" : "favorite_border"}</Icon>
+        </IconButton>
+      </Box>
+      <Typography variant="h5" textAlign="center">
+        This is script name
       </Typography>
     </>
   );
