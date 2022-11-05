7508d7d0d001c63a1c808d4da468755e0771b682
kaushalmeena <kaushal.meena1996@gmail.com>
2022-11-05 14:56:51 +0530

    chore: upgrade electron-forge to 6

---
+++ forge.config.ts
@@ -0,0 +1,37 @@
+import { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+
+import { mainConfig } from './webpack.main.config';
+import { rendererConfig } from './webpack.renderer.config';
+
+const config: ForgeConfig = {
+  packagerConfig: {
+    icon: "./assets/icons/icon"
+  },
+  rebuildConfig: {},
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  plugins: [
+    new WebpackPlugin({
+      mainConfig,
+      renderer: {
+        config: rendererConfig,
+        entryPoints: [
+          {
+            html: './src/renderer/index.html',
+            js: './src/renderer/index.tsx',
+            name: 'main_window',
+            preload: {
+              js: './src/preload/index.ts',
+            },
+          },
+        ],
+      },
+    }),
+  ],
+};
+
+export default config;
