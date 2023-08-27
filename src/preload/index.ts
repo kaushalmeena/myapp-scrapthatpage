6dd3df27d5e90cc5d812e9bdd3c36afd1c8f5bf5
kaushalmeena <kaushal.meena1996@gmail.com>
2023-08-27 12:49:26 +0530

    refactor(preload): move scraper declaration out

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,12 +1,4 @@
 import { contextBridge } from "electron";
 import { ScraperProxy } from "./proxy/scraper";
-import { ScraperAPI } from "./types/scraper";
-
-// This allow TypeScript to pick types for ScraperAPI on window
-declare global {
-  interface Window {
-    scraper: ScraperAPI;
-  }
-}
 
 contextBridge.exposeInMainWorld("scraper", ScraperProxy);
